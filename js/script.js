fd21eca56e0ccf7cc254f3c70ba628361c9c31e6
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-08-18 20:20:03 -0400

    Added css, js, html and readme files

--- js/script.js
+++ js/script.js
@@ -1,99 +1,62 @@
+const operators = document.querySelectorAll(".operators > div");
+const numbers = document.querySelectorAll(".numbers > div");
 const input = document.getElementById("input");
+const result = document.getElementById("result");
+let numsArr = [];
+let opsArr = [];
+let total = 0;
+let tempNum = ``;
 
-const numbers= Array.from (document.getElementsByClassName("numbers"));
+operators.forEach(item => {
+  item.addEventListener(`click`, e => {
+    numsArr.push(Number(tempNum));
+    opsArr.push(e.target.innerHTML);
+    tempNum = ``;
+  });
+});
 
-const operators = Array.from(document.querySelectorAll(".operators"));
-
-const equal = document.getElementById("result");
-
-
-// let numbersStr = '';
-
-
-//   for(let i = 0 ; i < numbers.length; i++) {
-//     let char = numbers[i];
-//     char.addEventListener(`click`, e => {
-//       if(e.target.id != `clear`) {
-//         numbersStr += e.target.textContent 
-//         input.textContent = numbersStr;
-//         console.log(Number(numbersStr))
-//       }else {
-//         numbersStr;
-//       }
-//     });
-//   };
-
-
-
-//   operators.forEach((operator, index) => {
-//     operator.addEventListener(`click`, e => {
-//       if(e.target.textContent == `+`){
-//         add(numbersStr,numbersStr);
-//       }
-//     });
-//   });
-
-
-
-// function add (num1,num2){
-  
-//   return (input.textContent = num1 + num2 )
-  
-// }
-
-// function subtract (num1,num2){
-//   let res = num1 - num2 ;
-//   return(resultArr.push(res))
-// }
-
-// function multiply (num1,num2){
-//   let res = num1 * num2 ;
-//   return(resultArr.push(res))
-// }
-
-// function divide (num1,num2){
-//   let res = num1 / num2 ;
-//   return(resultArr.push(res))
-// }
-numbers.forEach (button => {
-  
-  button.addEventListener(`click`, e => {
-
-    switch(e.target.innerText) {
-
-      case `C` : 
-        input.innerText = ``;
-        break;
-      
-      default :
-        input.innerText += e.target.innerText;
+numbers.forEach(item => {
+  item.addEventListener(`click`, e => {
+    if(e.target.textContent == `C`) {
+      input.textContent = 0 ;
+      tempNum = ``;
+      opsArr = [];
+      numsArr = [];
+      total = 0;
+    }else {
+      tempNum += item.innerHTML;
+      input.textContent = tempNum;
     };
   });
 });
 
-operators.forEach(button => {
-
-  button.addEventListener(`click`, operator => {
-    
-    switch(operator.target.innerText) {
-
-      default :
-        input.innerText += operator.target.innerText;
-
-    };
-  });
-});
-
-equal.addEventListener('click', () => {
-
-  try{
-
-    input.innerText = eval(input.innerText);
-
-  } catch {
-
-    input.innerText = `ERROR!`
-
+result.addEventListener(`click`, e => {
+  numsArr.push(Number(tempNum));
+  if(total == 0){
+    total += numsArr[0];
   };
-
+  for(let i = 0 ; i < opsArr.length; i++) {
+    doMath(total, numsArr[i + 1],opsArr[i]);
+  };
+  input.textContent = total;
+  numsArr = [];
+  opsArr = [];
 });
+
+function doMath(n1,n2,calc) {
+  switch(calc) {
+    case "+" :
+      total += n2;
+      break;
+    case '-' :
+      total -= n2;
+      break;
+    case '*' :
+      total *= n2;
+      break;
+    case '/' :
+      total /= n2;
+      break;    
+    default:
+  };
+};
